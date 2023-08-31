f6c6a89b0c043712cd09cf5b54f3990552e61906
Sean Jones <sean.d.jones92@gmail.com>
2023-08-31 16:22:24 -0600

    improve first time start

--- salt-lab/init.sh
+++ salt-lab/init.sh
@@ -4,4 +4,4 @@ docker compose up postgres -d
 docker compose up redis -d
 docker run --rm -it -v ./raas/:/etc/raas raas:latest save_creds
 docker run --rm -it -v ./raas/:/etc/raas raas:latest "dump --insecure --mode import < /tmp/sample-resource-types.raas"
-docker compose up -d
+docker compose down
--- salt-lab/reset.sh
+++ salt-lab/reset.sh
@@ -3,4 +3,5 @@
 rm -rf postgres/* 
 rm -rf raas/pki/ 
 rm -rf raas/raas.secconf 
+rm -rf raas/initialized
 rm -rf master/pki/
