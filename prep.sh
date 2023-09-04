f4fb90fe8a401aab3d625ff233a885815d5d53d2
Sean Jones <sean.d.jones92@gmail.com>
2023-09-03 18:01:33 -0600

    prep.sh sets postgres redis creds in env

--- prep.sh
+++ prep.sh
@@ -19,7 +19,7 @@ fi
 # clean the build dirs before every build prep
 rm -rf ./build/sse-installer ./build/raas/eapi_service ./build/salt-master/eapi_plugin .env
 
-# Check if the user has requested to onle clean up unpacked installer directories
+# Check if the user has requested to only clean up unpacked installer directories
 # if we are cleaning only then stop here and don't unpack again
 if [[ $1 == "--clean" ]]
 then
@@ -41,3 +41,21 @@ echo "RAAS_RPM_NAME=$RAAS_FILE_NAME" >> .env
 MASTER_PLUGIN_PATH=$(ls build/salt-master/eapi_plugin/files/SSEAPE*.whl)
 MASTER_PLUGIN_NAME=$(basename $MASTER_PLUGIN_PATH)
 echo "MASTER_PLUGIN_NAME=$MASTER_PLUGIN_NAME" >> .env
+
+read -p "use default postgres and redis credentials? (y/n) " use_defaults
+if [[ $use_defaults == "y" ]]
+then
+	echo "POSTGRES_USER=default" >> .env
+	echo "POSTGRES_PASS=postgres123" >> .env
+	echo "REDIS_PASS=redis123" >> .env
+elif [[ $use_defaults == "n" ]]
+then
+	read -p "postgres user: " POSTGRES_USER
+	read -p "postgres password: " POSTGRES_PASS
+	read -p "redis password: " REDIS_PASS
+	echo "POSTGRES_USER=${POSTGRES_USER}" >> .env
+	echo "POSTGRES_PASS=${POSTGRES_PASS}" >> .env
+	echo "REDIS_PASS=${REDIS_PASS}" >> .env
+else
+	echo "unsupported option"
+fi
